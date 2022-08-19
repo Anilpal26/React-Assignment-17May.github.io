1fdb4423c6cfa4b9c30c8a96f64e11ddf1222500
Anilpal26 <81358527+Anilpal26@users.noreply.github.com>
2022-08-19 16:11:11 +0530

    17-Aug 2nd task

---
+++ 17-Aug-2Assi/index.js
@@ -0,0 +1,31 @@
+console.log("New Assignment");
+
+let element1 = document.querySelector('#li1');
+let element2 = document.querySelector('#li2');
+let element3 = document.querySelector('#li3');
+let element4 = document.querySelector('#li4');
+let element5 = document.querySelector('#li5');
+let element6 = document.querySelector('#li6');
+
+if(element1.textContent == 'Item 1'){
+    element1.style.backgroundColor = "orange";
+}
+if(element2.textContent == 'Item 2'){
+    element2.style.backgroundColor = "orange";
+}
+if(element3.textContent == 'Item 3'){
+    element3.style.backgroundColor = "white";
+    element3.style.color = "blue";
+}
+if(element4.textContent == 'Item 4'){
+    element4.style.backgroundColor = "white";
+    element4.style.color = "blue";
+}
+if(element5.textContent == 'Item 5'){
+    element5.style.backgroundColor = "green";
+}
+if(element6.textContent == 'Item 6'){
+    element6.style.backgroundColor = "green";
+}
+ 
+
