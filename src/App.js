d33734d8fcc235d7a371d2b97d7851f3efc4af5b
Anilpal26 <81358527+Anilpal26@users.noreply.github.com>
2022-08-31 23:36:23 +0530

    24-Aug-Assi

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MERN Restorent</h1>
+      <img src='https://image.shutterstock.com/image-photo/restaurant-chilling-out-classy-lifestyle-260nw-507639565.jpg'/>
+    </div>
+  );
+}
+
+export default App;
