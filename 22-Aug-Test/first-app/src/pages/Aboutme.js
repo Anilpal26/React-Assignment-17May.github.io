86ba1135ce474e17171b229ace76c8faff7bdfd3
Anilpal26 <81358527+Anilpal26@users.noreply.github.com>
2022-09-01 00:36:43 +0530

    About me Page

---
+++ 22-Aug-Test/first-app/src/pages/Aboutme.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Aboutme = () => {
+  return (
+    <div>
+        <span>Name</span> : <span>Anil Pal</span><br/>
+        <span>Developer</span> : <span>MERN Stack</span><br/>
+        <span>City</span> : <span>Indore</span>
+    </div>
+  )
+}
+
+export default Aboutme;
